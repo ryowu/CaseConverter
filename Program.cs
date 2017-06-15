3357c958915e9f40288811189573dc3b71e89e41
ryowu <ryowu@msn.com>
2017-06-15 15:33:30 +0800

    version 1

--- Program.cs
+++ Program.cs
@@ -1,36 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace CaseConverter
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            //Get all excel files from current folder
-            List<string> files = GetExcelFiles();
-            
-            //Open each one by one, select * from sheet1 where type <> 'C' and action <> 'wait'
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//Get all excel files from current folder
+			List<string> files = Utility.GetExcelFiles();
 
-            //Translate into human language, store in DataTable
-            DataTable outputData = new DataTable();
+			//Create a new folder 'Output', if exist, skip
+			string folderName = "Output";
+			string outputFolder = Path.Combine(Application.StartupPath, folderName);
+			if (!Directory.Exists(outputFolder))
+			{
+				Directory.CreateDirectory(outputFolder);
+			}
 
-            //Create a new folder 'Output'
-            //Create new excel and insert the data
-            CreateOutputFiles(outputData);
-        }
+			//Open each one by one, select * from sheet1 where type <> 'C' and action <> 'wait'
+			DataTable outputData = new DataTable();
 
-        public static List<string> GetExcelFiles()
-        {
-            List<string> result = new List<string>();
-            return result;
-        }
+			files.ForEach(f =>
+			{
+				Utility.AddLog(string.Format("Open {0}", f));
+				//f is the full filepath and name
+				//read file to DataTable
+				DataTable inputMetadata = Utility.ReadInputFile(f);
+				
+				//Translate into human language, store in outputData
+				outputData = Utility.TranslateIntoHumanLanguage(inputMetadata);
 
-        public static void CreateOutputFiles(DataTable dt)
-        { }
-    }
+				//Create new excel and insert the data
+				Utility.CreateOutputFiles(outputData, outputFolder);
+				Utility.AddLog(string.Format("Complete {0}", f));
+			});
+		}
+	}
 }
---
+++ Utility.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaseConverter
+{
+	public class Utility
+	{
+		//log
+		private static StringBuilder sbLog = new StringBuilder();
+
+		public static List<string> GetExcelFiles()
+		{
+			List<string> result = new List<string>();
+			return result;
+		}
+
+		public static void CreateOutputFiles(DataTable dt, string outputFolder)
+		{
+		}
+
+		public static void AddLog(string s)
+		{
+			Console.WriteLine(s);
+			sbLog.AppendLine(s);
+		}
+
+		public static DataTable ReadInputFile(string filename)
+		{
+			DataTable result = new DataTable();
+			return result;
+		}
+
+		public static DataTable TranslateIntoHumanLanguage(DataTable inputData)
+		{
+			DataTable result = new DataTable();
+			return result;
+		}
+	}
+}
