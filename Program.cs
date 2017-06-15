2bb7d54bc6037d17b6fcacc5d1306c7758766dae
ryowu <ryowu@msn.com>
2017-06-15 15:15:00 +0800

    basic structure

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,26 @@ namespace CaseConverter
     {
         static void Main(string[] args)
         {
+            //Get all excel files from current folder
+            List<string> files = GetExcelFiles();
+            
+            //Open each one by one, select * from sheet1 where type <> 'C' and action <> 'wait'
+
+            //Translate into human language, store in DataTable
+            DataTable outputData = new DataTable();
+
+            //Create a new folder 'Output'
+            //Create new excel and insert the data
+            CreateOutputFiles(outputData);
         }
+
+        public static List<string> GetExcelFiles()
+        {
+            List<string> result = new List<string>();
+            return result;
+        }
+
+        public static void CreateOutputFiles(DataTable dt)
+        { }
     }
 }
