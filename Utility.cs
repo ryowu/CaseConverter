6338332e792554e04f6b721cd66bcaff37fa6cf0
ryowu <ryowu@msn.com>
2017-06-16 16:15:28 +0800

    add output method and template excel output file

--- Utility.cs
+++ Utility.cs
@@ -16,11 +16,75 @@ namespace CaseConverter
 
 		public static List<string> GetExcelFiles()
 		{
-            return Directory.GetFiles(Environment.CurrentDirectory, "*_SSD.xls").ToList();
+			return Directory.GetFiles(Environment.CurrentDirectory, "*_SSD.xls").ToList();
 		}
+
+		private static void ExportToExcel(DataTable dt, string filepath)
+		{
+
+			/*Set up work book, work sheets, and excel application*/
+			Microsoft.Office.Interop.Excel.Application oexcel = new Microsoft.Office.Interop.Excel.Application();
+			try
+			{
+				string path = AppDomain.CurrentDomain.BaseDirectory;
+				object misValue = System.Reflection.Missing.Value;
+				Microsoft.Office.Interop.Excel.Workbook obook = oexcel.Workbooks.Add(misValue);
+				Microsoft.Office.Interop.Excel.Worksheet osheet = new Microsoft.Office.Interop.Excel.Worksheet();
+
+
+				//  obook.Worksheets.Add(misValue);
+
+				osheet = (Microsoft.Office.Interop.Excel.Worksheet)obook.Sheets["Test"];
+				int colIndex = 0;
+				int rowIndex = 1;
+
+				foreach (DataColumn dc in dt.Columns)
+				{
+					colIndex++;
+					osheet.Cells[1, colIndex] = dc.ColumnName;
+				}
+				foreach (DataRow dr in dt.Rows)
+				{
+					rowIndex++;
+					colIndex = 0;
+
+					foreach (DataColumn dc in dt.Columns)
+					{
+						colIndex++;
+						osheet.Cells[rowIndex, colIndex] = dr[dc.ColumnName];
+					}
+				}
+
+				osheet.Columns.AutoFit();
+
+				//Release and terminate excel
+
+				obook.SaveAs(filepath);
+				obook.Close();
+				oexcel.Quit();
+				ReleaseObject(osheet);
+
+				ReleaseObject(obook);
+
+				ReleaseObject(oexcel);
+				GC.Collect();
+			}
+			catch (Exception ex)
+			{
+				oexcel.Quit();
+			}
+		}
+
+		private static void ReleaseObject(object o) { try { while (System.Runtime.InteropServices.Marshal.ReleaseComObject(o) > 0) { } } catch { } finally { o = null; } }
 
 		public static void CreateOutputFiles(DataTable dt, string outputFolder)
 		{
+			string outputfileBaseName = "outputBase.xls";
+			string outputFilename = Path.Combine(outputFolder, dt.TableName);
+			//create new file
+			File.Copy(outputfileBaseName, outputFilename);
+
+			ExportToExcel(dt, outputFilename);
 		}
 
 		public static void AddLog(string s)
@@ -29,196 +93,196 @@ namespace CaseConverter
 			sbLog.AppendLine(s);
 		}
 
-        public static void OutputLogToFile()
-        {
-            string logFile = "cc_" + DateTime.Now.ToString("s").Replace("-","").Replace(":","") + ".log";
-            if (File.Exists(logFile))
-            {
-                File.Delete(logFile);
-            }
-            try
-            {
-                using (StreamWriter sw = new StreamWriter(logFile))
-                {
-                    sw.Write(sbLog.ToString());
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Failed to write log in file.\n");
-                Console.WriteLine(ex.ToString());
-                Console.WriteLine();
-            }
-        }
+		public static void OutputLogToFile()
+		{
+			string logFile = "cc_" + DateTime.Now.ToString("s").Replace("-", "").Replace(":", "") + ".log";
+			if (File.Exists(logFile))
+			{
+				File.Delete(logFile);
+			}
+			try
+			{
+				using (StreamWriter sw = new StreamWriter(logFile))
+				{
+					sw.Write(sbLog.ToString());
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("Failed to write log in file.\n");
+				Console.WriteLine(ex.ToString());
+				Console.WriteLine();
+			}
+		}
 
-        /// <summary>
-        /// Open each one by one, select * from sheet1
-        /// </summary>
-        /// <param name="filename"></param>
-        /// <returns></returns>
-        public static DataTable ReadInputFile(string filename)
-        {
-            var excelConnectionString = GetExcelConnectionString(filename);
-            var dataTable = new DataTable();
+		/// <summary>
+		/// Open each one by one, select * from sheet1
+		/// </summary>
+		/// <param name="filename"></param>
+		/// <returns></returns>
+		public static DataTable ReadInputFile(string filename)
+		{
+			var excelConnectionString = GetExcelConnectionString(filename);
+			var dataTable = new DataTable();
 
-            using (var excelConnection = new OleDbConnection(excelConnectionString))
-            {
-                excelConnection.Open();
-                var dataAdapter = new OleDbDataAdapter("SELECT * FROM [Test$]", excelConnection);
-                dataAdapter.Fill(dataTable);
-                excelConnection.Close();
-            }
-            AddLog("OpenExcelFile: File successfully opened:" + filename);
+			using (var excelConnection = new OleDbConnection(excelConnectionString))
+			{
+				excelConnection.Open();
+				var dataAdapter = new OleDbDataAdapter("SELECT * FROM [Test$]", excelConnection);
+				dataAdapter.Fill(dataTable);
+				excelConnection.Close();
+			}
+			AddLog("OpenExcelFile: File successfully opened:" + filename);
 
-            return dataTable;
-        }
+			return dataTable;
+		}
 
-        public static DataTable TranslateIntoHumanLanguage(DataTable inputData)
-        {
-            DataTable result = new DataTable();
-            result.Columns.Add(new DataColumn("steps", typeof(string)));
-            result.Columns.Add(new DataColumn("verifications", typeof(string)));
+		public static DataTable TranslateIntoHumanLanguage(DataTable inputData)
+		{
+			DataTable result = new DataTable();
+			result.Columns.Add(new DataColumn("steps", typeof(string)));
+			result.Columns.Add(new DataColumn("verifications", typeof(string)));
 
-            StringBuilder sbSteps = new StringBuilder();
-            StringBuilder sbVerifications = new StringBuilder();
-            string col4LowerValue = null;
-            string col5RawValue = null;
-            string col6RawValue = null;
-            string col6TranslatedValue = null;
-            bool stepFlag = false;  // true means last row is for steps
-            bool verifyFlag = false; // true means last row is of verifications
+			StringBuilder sbSteps = new StringBuilder();
+			StringBuilder sbVerifications = new StringBuilder();
+			string col4LowerValue = null;
+			string col5RawValue = null;
+			string col6RawValue = null;
+			string col6TranslatedValue = null;
+			bool stepFlag = false;  // true means last row is for steps
+			bool verifyFlag = false; // true means last row is of verifications
 
-            foreach (DataRow row in inputData.Rows)
-            {
-                col4LowerValue = row[4].ToString().Trim().ToLower();
+			foreach (DataRow row in inputData.Rows)
+			{
+				col4LowerValue = row[4].ToString().Trim().ToLower();
 
-                #region skip specific rows
-                if (row[1].ToString().Trim().Equals("C",StringComparison.OrdinalIgnoreCase) ||
-                    col4LowerValue.StartsWith("wait") ||
-                    col4LowerValue.StartsWith("handleajax") ||
-                    col4LowerValue.StartsWith("basicmode") ||
-                    col4LowerValue.StartsWith("closebrowser") ||
-                    col4LowerValue.StartsWith("windowclose") ||
-                    col4LowerValue.StartsWith("comment") ||
-                    col4LowerValue.StartsWith("portal") ||
-                    col4LowerValue.StartsWith("attach"))
-                {
-                    continue;
-                }
-                #endregion
+				#region skip specific rows
+				if (row[1].ToString().Trim().Equals("C", StringComparison.OrdinalIgnoreCase) ||
+					col4LowerValue.StartsWith("wait") ||
+					col4LowerValue.StartsWith("handleajax") ||
+					col4LowerValue.StartsWith("basicmode") ||
+					col4LowerValue.StartsWith("closebrowser") ||
+					col4LowerValue.StartsWith("windowclose") ||
+					col4LowerValue.StartsWith("comment") ||
+					col4LowerValue.StartsWith("portal") ||
+					col4LowerValue.StartsWith("attach"))
+				{
+					continue;
+				}
+				#endregion
 
-                col5RawValue = row[5].ToString();
-                col6RawValue = row[6].ToString();
-                col6TranslatedValue = TranslateColumn5Value(col6RawValue);
+				col5RawValue = row[5].ToString();
+				col6RawValue = row[6].ToString();
+				col6TranslatedValue = TranslateColumn5Value(col6RawValue);
 
-                #region verifications
-                if (col4LowerValue.Equals("Verify", StringComparison.OrdinalIgnoreCase))
-                {
-                    sbVerifications.AppendLine(TranslateCheckPointString(col5RawValue, col6RawValue));
-                    if (stepFlag)
-                    {
-                        // insert all step strings
-                        result.Rows.Add(sbSteps.ToString(), "");
-                        sbSteps.Clear();
-                    }
-                    verifyFlag = true;
-                    stepFlag = false;
-                }
-                #endregion
-                #region steps
-                else
-                {
-                    if (col4LowerValue.Equals("LaunchApp", StringComparison.OrdinalIgnoreCase))
-                    {
-                        sbSteps.AppendLine("Launch Aprimo Marketing");
-                    }
-                    else if (col4LowerValue.Equals("LaunchPortal", StringComparison.OrdinalIgnoreCase))
-                    {
-                        sbSteps.AppendLine("Launch Aprimo Portal");
-                    }
-                    else if (col4LowerValue.Equals("LogOutExit", StringComparison.OrdinalIgnoreCase))
-                    {
-                        sbSteps.AppendLine("Log off current user");
-                    }
-                    else if (col4LowerValue.Equals("EnterData", StringComparison.OrdinalIgnoreCase))
-                    {
-                        sbSteps.AppendLine("Enter data, set \"" + col5RawValue + "\" as \"" + col6TranslatedValue + "\"");
-                    }
-                    else if (col4LowerValue.Equals("EnterComboText", StringComparison.OrdinalIgnoreCase))
-                    {
-                        sbSteps.AppendLine("Select combobox, set \"" + col5RawValue + "\" as \"" + col6TranslatedValue + "\"");
-                    }
-                    else if (col4LowerValue.StartsWith("ClickItem", StringComparison.OrdinalIgnoreCase))
-                    {
-                        sbSteps.AppendLine("Click \"" + col5RawValue + "\"");
-                    }
-                    else if (col4LowerValue.Equals("SelectItem", StringComparison.OrdinalIgnoreCase))
-                    {
-                        sbSteps.AppendLine("Select \"" + col5RawValue + "\"");
-                    }
-                    else
-                    {
-                        throw new Exception("Unhandled col4LowerValue:" + col4LowerValue);
-                    }
+				#region verifications
+				if (col4LowerValue.Equals("Verify", StringComparison.OrdinalIgnoreCase))
+				{
+					sbVerifications.AppendLine(TranslateCheckPointString(col5RawValue, col6RawValue));
+					if (stepFlag)
+					{
+						// insert all step strings
+						result.Rows.Add(sbSteps.ToString(), "");
+						sbSteps.Clear();
+					}
+					verifyFlag = true;
+					stepFlag = false;
+				}
+				#endregion
+				#region steps
+				else
+				{
+					if (col4LowerValue.Equals("LaunchApp", StringComparison.OrdinalIgnoreCase))
+					{
+						sbSteps.AppendLine("Launch Aprimo Marketing");
+					}
+					else if (col4LowerValue.Equals("LaunchPortal", StringComparison.OrdinalIgnoreCase))
+					{
+						sbSteps.AppendLine("Launch Aprimo Portal");
+					}
+					else if (col4LowerValue.Equals("LogOutExit", StringComparison.OrdinalIgnoreCase))
+					{
+						sbSteps.AppendLine("Log off current user");
+					}
+					else if (col4LowerValue.Equals("EnterData", StringComparison.OrdinalIgnoreCase))
+					{
+						sbSteps.AppendLine("Enter data, set \"" + col5RawValue + "\" as \"" + col6TranslatedValue + "\"");
+					}
+					else if (col4LowerValue.Equals("EnterComboText", StringComparison.OrdinalIgnoreCase))
+					{
+						sbSteps.AppendLine("Select combobox, set \"" + col5RawValue + "\" as \"" + col6TranslatedValue + "\"");
+					}
+					else if (col4LowerValue.StartsWith("ClickItem", StringComparison.OrdinalIgnoreCase))
+					{
+						sbSteps.AppendLine("Click \"" + col5RawValue + "\"");
+					}
+					else if (col4LowerValue.Equals("SelectItem", StringComparison.OrdinalIgnoreCase))
+					{
+						sbSteps.AppendLine("Select \"" + col5RawValue + "\"");
+					}
+					else
+					{
+						throw new Exception("Unhandled col4LowerValue:" + col4LowerValue);
+					}
 
-                    if (verifyFlag)
-                    {
-                        // insert all verification strings
-                        result.Rows[result.Rows.Count - 1][1] = sbVerifications.ToString();
-                        sbVerifications.Clear();
-                    }
-                    verifyFlag = false;
-                    stepFlag = true;
-                }
-                #endregion
-            }
-            return result;
-        }
+					if (verifyFlag)
+					{
+						// insert all verification strings
+						result.Rows[result.Rows.Count - 1][1] = sbVerifications.ToString();
+						sbVerifications.Clear();
+					}
+					verifyFlag = false;
+					stepFlag = true;
+				}
+				#endregion
+			}
+			return result;
+		}
 
-        private static string TranslateColumn5Value(string input)
-        {
-            string result = input;
-            int index = result.IndexOf('|');
-            if (index >= 0)
-            {
-                result = result.Substring(index + 1);
-            }
-            result = result.Replace("<$", "").Replace("$>", "");
-            return result;
-        }
+		private static string TranslateColumn5Value(string input)
+		{
+			string result = input;
+			int index = result.IndexOf('|');
+			if (index >= 0)
+			{
+				result = result.Substring(index + 1);
+			}
+			result = result.Replace("<$", "").Replace("$>", "");
+			return result;
+		}
 
-        private static string TranslateCheckPointString(string col5Value, string col6Value)
-        {
-            string[] array = col6Value.Split(':');
-            string result = null;
-            if (array.Length > 1)
-            {
-                result = array[0] + ": " + TranslateColumn5Value(array[1]);
-            }
-            else
-            {
-                result = array[0];
-            }
-            string tmp = col5Value;
-            if (col5Value.Equals("PageContent", StringComparison.OrdinalIgnoreCase))
-            {
-                tmp = "page content";
-            }
-            else if (col5Value.Equals("PageTitle", StringComparison.OrdinalIgnoreCase))
-            {
-                tmp = "page title";
-            }
+		private static string TranslateCheckPointString(string col5Value, string col6Value)
+		{
+			string[] array = col6Value.Split(':');
+			string result = null;
+			if (array.Length > 1)
+			{
+				result = array[0] + ": " + TranslateColumn5Value(array[1]);
+			}
+			else
+			{
+				result = array[0];
+			}
+			string tmp = col5Value;
+			if (col5Value.Equals("PageContent", StringComparison.OrdinalIgnoreCase))
+			{
+				tmp = "page content";
+			}
+			else if (col5Value.Equals("PageTitle", StringComparison.OrdinalIgnoreCase))
+			{
+				tmp = "page title";
+			}
 
-            return "Verify " + tmp + ": " + result;
-        }
+			return "Verify " + tmp + ": " + result;
+		}
 
-        private static string GetExcelConnectionString(string fileName)
-        {
-            //return "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + fileName + ";Extended Properties=Excel 12.0;";
-            return @"Provider=Microsoft.Jet.OLEDB.4.0;" +
-                   @"Data Source=" + fileName + ";" +
-                   @"Extended Properties=" + Convert.ToChar(34).ToString() +
-                   @"Excel 8.0" + Convert.ToChar(34).ToString() + ";";
-        }
-    }
+		private static string GetExcelConnectionString(string fileName)
+		{
+			//return "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + fileName + ";Extended Properties=Excel 12.0;";
+			return @"Provider=Microsoft.Jet.OLEDB.4.0;" +
+				   @"Data Source=" + fileName + ";" +
+				   @"Extended Properties=" + Convert.ToChar(34).ToString() +
+				   @"Excel 8.0" + Convert.ToChar(34).ToString() + ";";
+		}
+	}
 }
